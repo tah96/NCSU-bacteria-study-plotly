8b260eb2d5b5cb345b28460ffd0f067fbe509941
tah96 <tylerandrewhunt96@gmail.com>
2020-12-21 20:47:43 -0500

    more changes

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,31 +2,35 @@
 json_file = "/data/samples.json"
 
 // Fetch the JSON data and console log it
+//var metaDataList = []
 d3.json(json_file).then(function(data) {
   var metaData = data.metadata;
   var sampleData = data.samples;
+  //metaDataList.push(metaData);
   console.log(sampleData);
   console.log(metaData);
 
-  var dropdown_list = d3.select(".dropdown-menu");
+  var dropdown_list = d3.select("#selDataset");
   var demographic = d3.select('#sample-metadata');
   var bar_chart = d3.select('#bar');
   var bubble_chart = d3.select('#bubble');
-
-  metaData.forEach((patients) => {
-    inside_list = patients.id;
-    var list = dropdown_list.append("li")
-      list.text(inside_list)
-  });    
+  
+  var default_patient_id = "955"
 
   // Filter to Select a Certain Patient to be used later with user input
   function defaultPatient(person) {
-    return person.id === "955"
+    return person.id === default_patient_id
   };
   
   var patient = sampleData.filter(defaultPatient);
 
   function init() {
+    metaData.forEach((patients) => {
+      inside_list = patients.id;
+      var list = dropdown_list.append("option").property("value", patients)
+        list.text(inside_list)
+    });
+
     var patient_otu_ids_text = patient.map((otu) => {
       var text_otu_ids = []
       var raw_ids = otu.otu_ids
@@ -51,7 +55,7 @@ d3.json(json_file).then(function(data) {
 
     var correct_patient = []
     var patientMetaData = metaData.forEach((patient)=> {
-      if (patient.id === 955) {
+      if (patient.id === parseInt(default_patient_id)) {
         var patient_info = patient;
         correct_patient.push(patient_info)
       }
@@ -114,112 +118,110 @@ d3.json(json_file).then(function(data) {
       Plotly.newPlot("bubble", bubble_plot_data, bubble_layout);
   };
   init();
-
-  dropdown_list.on("change",runPatient)
-
-  function runPatient() {
-
-    // Prevent the page from refreshing
-    d3.event.preventDefault();
-    // Select the input element and get the raw HTML node
-    var inputPatient = d3.select(this); //.select("input");
-    console.log(inputPatient);
-
-    function selectedPatient(person) {
-      return person.id === `${inputPatient}`
-    }
-
-    var patient = sampleData.filter(selectedPatient);
-
-    var patient_otu_ids_text = patient.map((otu) => {
-      var text_otu_ids = []
-      var raw_ids = otu.otu_ids
-      raw_ids.forEach((id) => {
-        var string = id.toString();
-        text_otu_ids.push(`OTU ${string}`)
-      });
-      return text_otu_ids
-    });
-  
-    var patient_otu_ids = patient.map((otu) => {
-      return otu.otu_ids
-    });
-  
-    var patient_otu_samples = patient.map((otu) => {
-      return otu.sample_values
-    });
-  
-    var patient_otu_labels = patient.map((otu) => {
-      return otu.otu_labels
-    });
-
-    //demographic.html("")
-    //bar_chart.html("")
-    //bubble_chart.html("")
-
-    var correct_patient = []
-    var patientMetaData = metaData.forEach((patient)=> {
-      if (patient.id === inputPatient) {
-        var patient_info = patient;
-        correct_patient.push(patient_info)
-      }
-    });
-    console.log(correct_patient);
-
-    correct_patient.forEach((instance) => {
-      // Step 3: Use 'Object.entries' to capture each UFO sighting key and value
-      Object.entries(instance).forEach(([key, value]) => {
-        var text = demographic.append("li");
-        text.text(`${key}: ${value}`)
-      });  
-    });        
-  
-    var bar_plot_data = [{
-      x: patient_otu_samples[0].slice(0,10),
-      y: patient_otu_ids_text[0].slice(0,10),
-      type: "bar",
-      text: patient_otu_labels[0].slice(0,10),
-      orientation: "h"
-    }];
-
-    var layout = {
-        title: `Bar Chart: Patient ${inputPatient}`,
-        xaxis: {
-          title: "Bacteria Count"
-        },
-        yaxis: {
-          title: "ID"
-        }
-    };
-  
-    Plotly.newPlot("bar", bar_plot_data, layout);
-
-    var bubble_plot_data = [{
-      x: patient_otu_ids[0],
-      y: patient_otu_samples[0],
-      text: patient_otu_labels[0],
-      mode: 'markers',
-      marker: {
-        color: patient_otu_ids[0],
-        opacity: 0.8,
-        size: patient_otu_samples[0]
-        }
-      }];
-      
-    var bubble_layout = {
-        title: `Bubble Chart Patient ${inputPatient}`,
-        xaxis: {
-          title: 'OTU ID'
-        },
-        yaxis: {
-          title: 'Count in Subject/Patient'
-        },
-        showlegend: false,
-        height: 500,
-        width: 1200,
-      };
-      
-    Plotly.newPlot("bubble", bubble_plot_data, bubble_layout);
-  }
 });
 
+//dropdown_list.on("change",optionChanged)
+
+function optionChanged(new_id) {
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  // Select the input element and get the raw HTML node
+  console.log(new_id);
+  var inputPatient = new_id;
+  console.log(inputPatient);
+
+  function selectedPatient(person) {
+    return person.id === `${inputPatient}`
+  }
+
+  var patient = sampleData.filter(selectedPatient);
+
+  var patient_otu_ids_text = patient.map((otu) => {
+    var text_otu_ids = []
+    var raw_ids = otu.otu_ids
+    raw_ids.forEach((id) => {
+      var string = id.toString();
+      text_otu_ids.push(`OTU ${string}`)
+    });
+    return text_otu_ids
+  });
+
+  var patient_otu_ids = patient.map((otu) => {
+    return otu.otu_ids
+  });
+
+  var patient_otu_samples = patient.map((otu) => {
+    return otu.sample_values
+  });
+
+  var patient_otu_labels = patient.map((otu) => {
+    return otu.otu_labels
+  });
+
+  //demographic.html("")
+  //bar_chart.html("")
+  //bubble_chart.html("")
+
+  var correct_patient = []
+  var patientMetaData = metaData.forEach((patient)=> {
+    if (patient.id === inputPatient) {
+      var patient_info = patient;
+      correct_patient.push(patient_info)
+    }
+  });
+  console.log(correct_patient);
+
+  correct_patient.forEach((instance) => {
+    // Step 3: Use 'Object.entries' to capture each UFO sighting key and value
+    Object.entries(instance).forEach(([key, value]) => {
+      var text = demographic.append("li");
+      text.text(`${key}: ${value}`)
+    });  
+  });        
+
+  var bar_plot_data = [{
+    x: patient_otu_samples[0].slice(0,10),
+    y: patient_otu_ids_text[0].slice(0,10),
+    type: "bar",
+    text: patient_otu_labels[0].slice(0,10),
+    orientation: "h"
+  }];
+
+  var layout = {
+    title: `Bar Chart: Patient ${inputPatient}`,
+    xaxis: {
+      title: "Bacteria Count"
+    },
+    yaxis: {
+      title: "ID"
+    }
+  };
+
+  Plotly.newPlot("bar", bar_plot_data, layout);
+
+  var bubble_plot_data = [{
+    x: patient_otu_ids[0],
+    y: patient_otu_samples[0],
+    text: patient_otu_labels[0],
+    mode: 'markers',
+    marker: {
+      color: patient_otu_ids[0],
+      opacity: 0.8,
+      size: patient_otu_samples[0]
+      }
+  }];
+    
+  var bubble_layout = {
+    title: `Bubble Chart Patient ${inputPatient}`,
+    xaxis: {
+      title: 'OTU ID'
+    },
+    yaxis: {
+      title: 'Count in Subject/Patient'
+    },
+    showlegend: false,
+    height: 500,
+    width: 1200
+  }; 
+  Plotly.newPlot("bubble", bubble_plot_data, bubble_layout);
+};
